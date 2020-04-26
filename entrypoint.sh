9b4912a1ceb69a985fecb4ffa4b7f8384fa494fd
rubenpve <rverdugo@travix.com>
2020-04-26 16:52:25 +0100

    Improve waiting for port-forward

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,7 @@ fi
 envsubst < /.kube/config_template > /.kube/config
 
 kubectl port-forward -n ${NAMESPACE} svc/${DB_SVC} ${LOCAL_PORT}:${DB_SVC_PORT}&
-#TODO improve waiting
-echo "Waiting for port-forward to work"
-sleep 10
+
 
 flyway -user="${DB_USER}" -password="${DB_PASSWORD}" ${BASELINE} -url=jdbc:${DB_ENGINE}://localhost:"${LOCAL_PORT}"/"${DB_NAME}" -schemas="${DB_NAME}" \
- -locations="${SCRIPT_LOCATIONS}" migrate
+ -locations="${SCRIPT_LOCATIONS}" -connectRetries=20 migrate
